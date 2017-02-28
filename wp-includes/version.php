d8e3b641f1431f14fc24d89808d9395fc79c05c5
humanit-se <>
2017-02-28 11:33:41 +0100

    Version 4.0.4

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0.3';
+$wp_version = '4.0.4';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 29630;
+$wp_db_version = 29631;
 
 /**
  * Holds the TinyMCE version
