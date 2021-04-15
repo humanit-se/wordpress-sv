a0f896d084a9618635503c65c9e987b18a2132ec
humanit-se <>
2021-04-15 09:17:55 +0200

    Version 4.8.16

--- wp-includes/Requests/Utility/FilteredIterator.php
+++ wp-includes/Requests/Utility/FilteredIterator.php
@@ -43,16 +43,4 @@ class Requests_Utility_FilteredIterator extends ArrayIterator {
 		return $value;
 	}
 
-	/**
-	 * @inheritdoc
-	 */
-	public function unserialize( $serialized ) {
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function __unserialize( $serialized ) { // phpcs:ignore PHPCompatibility.FunctionNameRestrictions.ReservedFunctionNames.MethodDoubleUnderscore,PHPCompatibility.FunctionNameRestrictions.NewMagicMethods.__unserializeFound
-		$this->unserialize( $serialized );
-	}
 }
